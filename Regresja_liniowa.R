a41bd64a4c3756ba3b0fe39312af6397e9d659b7
danieln96 <niczyporuk96@interia.pl>
2017-06-22 18:35:01 +0200

    Regresja

--- Regresja_liniowa.R
+++ Regresja_liniowa.R
@@ -15,19 +15,19 @@ test$Y <- as.factor(test$Y)
 ########### Regresja Liniowa ############
 
 #Damian
-RegLin = lm(Y~X2+X8+X15, data = train)
+#RegLin = lm(Y~X1+X6+X11+X16+X21, data = train)
 
 #Daniel
-RegLin = lm(Y~X7+X9+X11, data = train)
+#RegLin = lm(Y~X2+X7+X12+X17+X22, data = train)
 
 #Mateusz
-RegLin = lm(Y~X3+X6+X16, data = train)
+#RegLin = lm(Y~X3+X8+X13+X18+X23, data = train)
 
 #Jakub
-RegLin = lm(Y~X10+X12+X14, data = train)
+#RegLin = lm(Y~X4+X9+X14+X19, data = train)
 
 #Dawid
-RegLin = lm(Y~X10+X12+X14, data = train)
+#RegLin = lm(Y~X5+X10+X15+X20, data = train)
 
 #Ca³oœæ
 RegLin = lm(Y~X1+X2+X3+X4+X5+X6+X7+X8+X9+X10+X11+X12+X13+X14+X15+X16+X17+X18+X19+X20+X21+X22+X23, data = train)
